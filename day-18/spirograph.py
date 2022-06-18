88b5bd85da88083d68a9e5ee6a87e946d2425fd6
Ahmet Aydın <birceste@gmail.com>
2022-06-18 21:15:23 +0300

    Create spirograph.py

---
+++ day-18/spirograph.py
@@ -0,0 +1,29 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+tim.speed("fastest")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(150)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(8)
+
+screen = t.Screen()
+screen.exitonclick()
